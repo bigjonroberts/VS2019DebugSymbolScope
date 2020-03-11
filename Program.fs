78b6fcf1dcf500d6d14462d43378e28a0cb0e500
Jon Roberts <bigjonroberts@users.noreply.github.com>
2020-03-11 13:10:19 -0500

    initial commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿
+let z = 42
+
+let addTwice x y =
+    let z = x + y
+    printfn "Value inside function scope: %i" z
+    x + y + z
+
+[<EntryPoint>]
+let main _ =
+
+    addTwice 4 2 |> printfn "Function result: %i"
+    printfn "Value outside function scope: %i" z
+
+    0
